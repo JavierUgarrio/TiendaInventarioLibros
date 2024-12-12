c82e3c3d31ca058f84934290a78cc5057a8d00fd
JaviUga <j.ugarrio.duro@gmail.com>
2024-12-12 12:25:59 +0100

    servicio de obtener categorias

---
+++ src/main/java/com/company/tienda/response/RespuestaCategoriaRest.java
@@ -0,0 +1,13 @@
+package com.company.tienda.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RespuestaCategoriaRest extends RespuestaRest{
+
+	private RespuestaCategoria repuestaCategoria = new RespuestaCategoria();
+	
+	
+}
